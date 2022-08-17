58b969953ba4317036bf86155812564a4d6206da
Abdullah Fikri Handi Saputra <abdullahfikrihandi@gmail.com>
2022-08-17 19:40:06 +0700

    Delete test directory

--- test/belajar_dart_collection_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:belajar_dart_collection/belajar_dart_collection.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
